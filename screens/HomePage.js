bf34019c076eef0aed92bead05d214d2f1da92ed
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2023-02-03 23:51:10 +0700

    1st setup complete

---
+++ screens/HomePage.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Test from '../components/Test'
+
+
+const HomePage = () => {
+  return (
+    <Test />
+  )
+}
+
+export default HomePage
